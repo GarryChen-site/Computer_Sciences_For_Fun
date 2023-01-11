77549f762ae11c7c9a1a425ce14f00bba0f4fb94
chenyong <chenyong@djcps.com>
2023-01-11 08:18:48 +0800

    Fetch-and-Modify Operations

---
+++ rust_atomics_and_locks/examples/ch2-03-progress-reporting-unpark.rs
@@ -0,0 +1,36 @@
+use std::{sync::atomic::AtomicUsize, thread, time::Duration};
+use std::sync::atomic::Ordering::Relaxed;
+
+
+fn main() {
+	let num_done = AtomicUsize::new(0);
+
+	let main_thread = thread::current();
+
+	thread::scope(|s| {
+		// A background thread to process all 100 items
+		s.spawn(|| {
+			for  i in 0..100 {
+				process_item(i); // Assuming this takes some time
+				num_done.store(i + 1, Relaxed);
+				main_thread.unpark(); // Wake up the main thread
+			}
+		});
+
+		// The main thread shows status updates, every second
+		loop {
+			let n = num_done.load(Relaxed);
+			if n == 100 {
+				break;
+			}
+			println!("Working.. {n}/100 done");
+			thread::park_timeout(Duration::from_secs(1));
+		}
+	});
+	println!("Done!");
+}
+
+
+fn process_item(_: usize) {
+	thread::sleep(Duration::from_millis(37));
+}
