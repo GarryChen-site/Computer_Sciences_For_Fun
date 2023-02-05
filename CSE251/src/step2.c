63ae1204ca23b829f24df3756461c498860996d6
GarryChen <chenymj23@gmail.com>
2023-02-05 21:16:40 +0800

    c env and rust pass chap5

---
+++ CSE251/src/step2.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+/*
+ * Name : (your name here)
+ * 
+ * This is my first CSE 251 C program!!!
+ */
+
+
+int main()
+{
+    printf("My first C program\n");
+}
--- rust_atomics_and_locks/src/ch5_channels/mod.rs
+++ rust_atomics_and_locks/src/ch5_channels/mod.rs
@@ -4,3 +4,4 @@ pub mod s3_checks;
 pub mod s3_single_atomic;
 pub mod s4_types;
 pub mod s5_borrowing;
+pub mod s6_blocking;
