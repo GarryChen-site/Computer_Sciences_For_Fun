48d0f0155d747095bb0e3b09ca07163f87e4b9e8
GarryChen <chenymj23@gmail.com>
2024-02-22 08:11:43 +0800

    project2-partA-task4,5

--- cs61c/sp22-proj2-starter/src/dot.s
+++ cs61c/sp22-proj2-starter/src/dot.s
@@ -19,26 +19,45 @@
 # =======================================================
 dot:
 
-	# Prologue
+	li t0, 1 # error detection
+	blt a2, t0, array_len_err # if array length is less than 1, terminate the program with error code 36
+	blt a3, t0, stride_len_err # if stride of arr0 is less than 1, terminate the program with error code 37
+	blt a4, t0, stride_len_err # if stride of arr1 is less than 1, terminate the program with error code 37
+	mv t0, x0 # reset counter to 0
+	mv t5, x0 # reset result to 0
 
 
 loop_start:
+	# process the first element
+	mul t1, a3, t0 # step stride
+	slli t1, t1, 2 # convert to byte
+	add t1, a0, t1 # get the address of the element
+	lw t3, 0(t1) # load the element
 
+	# process the second element
+	mul t2, a4, t0 # step stride
+	slli t2, t2, 2 # convert to byte
+	add t2, a1, t2 # get the address of the element
+	lw t4, 0(t2) # load the element
 
+	# sum
+	mul t3, t3, t4 # reuse t3 to store the product
+	add t5, t5, t3 # accumulate the result
+	addi t0, t0, 1 # increment counter
+	bge t0, a2, loop_end # if counter >= a2, terminate the loop
+	j loop_start # otherwise, continue the loop
 
-
-
-
-
-
-
-
+array_len_err:
+    li      a0, 36
+    j       exit
+stride_len_err:
+    li      a0, 37
+    j       exit
 
 
 loop_end:
 
-
 	# Epilogue
 
-
+	mv 	a0, t5
 	ret
--- cs61c/sp22-proj2-starter/src/matmul.s
+++ cs61c/sp22-proj2-starter/src/matmul.s
@@ -26,38 +26,94 @@
 matmul:
 
 	# Error checks
-
+    li      t0, 1
+    blt     a1, t0, error
+    blt     a2, t0, error
+    blt     a4, t0, error
+    blt     a5, t0, error
+    bne     a2, a4, error
+	bne 	a1, a5, error
 
 	# Prologue
+    addi   sp, sp, -32
+    sw     ra, 28(sp)
+    sw     s6, 24(sp)
+    sw     s5, 20(sp)
+    sw     s4, 16(sp)
+    sw     s3, 12(sp)
+    sw     s2, 8(sp)
+    sw     s1, 4(sp)
+    sw     s0, 0(sp)
+
+    mv     s0, a0
+    mv     s1, a1
+    mv     s2, a2
+    mv     s3, a3
+    mv     s4, a4
+    mv     s5, a5
+    mv     s6, a6
+
+    # init
+    li      t0, 0  # i=0
 
 
 outer_loop_start:
-
-
-
+    li      t1, 0  # j=0
 
 inner_loop_start:
+	mv a0, s0
+    
+    # calculate a1 address
+    li t2, 4
+    mul t2, t2, t1
+    add a1, s3, t2
 
+    mv a2, s2
+    li a3, 1
+    mv a4, s5
+    
+    # prologue
+	addi sp, sp, -8
+    sw t0, 0(sp)
+    sw t1, 4(sp)
+    
+    jal dot # dot's args are in a0, a1, a2, a3, a4
 
-
-
-
-
-
-
-
-
-
+    sw a0, 0(s6)
+    addi s6, s6, 4
+    
+    lw t0, 0(sp)
+    lw t1, 4(sp)
+	addi sp, sp, 8
+    # epilogue
+    
+    addi t1, t1, 1 # j++
+    beq t1, s5, inner_loop_end
+    j inner_loop_start
 
 inner_loop_end:
+	addi t0, t0, 1 # i++
+    beq t0, s1, outer_loop_end
 
+	li t2, 4
+    mul t2, t2, s2
+    add s0, s0, t2
 
-
-
+    j outer_loop_start
 outer_loop_end:
 
-
 	# Epilogue
-
-
+    lw     ra, 28(sp)
+    lw     s6, 24(sp)
+    lw     s5, 20(sp)
+    lw     s4, 16(sp)
+    lw     s3, 12(sp)
+    lw     s2, 8(sp)
+    lw     s1, 4(sp)
+    lw     s0, 0(sp)
+    addi   sp, sp, 32
 	ret
+
+error:
+    li      a0, 38
+    j       exit
