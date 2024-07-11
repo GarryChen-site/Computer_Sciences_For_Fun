dbc61d3e3d2e5d554e59ca6e6b28ea81c7874863
GarryChen <chenymj23@gmail.com>
2024-07-11 20:38:53 +0800

    finish ch2-09

---
+++ eopl3/ch2/09.scm
@@ -0,0 +1,46 @@
+(load "../libs/init.scm")
+
+
+(define empty-env
+  (lambda() '()))
+
+(define extend-env
+  (lambda (var val env)
+    (cons (cons var val)
+          env)))
+
+(define apply-env
+  (lambda (env search-var)
+    (cond
+     ((null? env)
+      (report-no-binding-found search-var))
+     ((eqv? (caar env) search-var)
+      (cdr (car env)))
+     (else
+      (apply-env (cdr env) search-var)))))
+
+(define report-no-binding-found
+  (lambda (search-var)
+    (error 'apply-env "No binding for: " search-var)))
+
+
+(define has-binding?
+  (lambda (env var)
+    (cond
+     ((null? env) #f)
+     ((eqv? (caar env) var) #t)
+     (else (has-binding? (cdr env) var)))))
+
+(define e
+  (extend-env 'd 6
+     (extend-env 'y 8
+        (extend-env 'x 7
+           (extend-env 'y 14
+              (empty-env))))))
+
+(equal?? (has-binding? e 'd) #t)
+(equal?? (has-binding? e 'y) #t)
+(equal?? (has-binding? e 'x) #t)
+(equal?? (has-binding? e 'z) #f)
+
+(exit)
