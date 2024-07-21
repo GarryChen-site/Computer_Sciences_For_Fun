28833b0f2a6f7be36a2f401ee87fdd1b62e749fd
GarryChen <chenymj23@gmail.com>
2024-07-21 21:06:35 +0800

    2.2 finish

---
+++ eopl3/ch2/13.scm
@@ -0,0 +1,51 @@
+(load "../libs/init.scm")
+
+
+(define empty-env
+  (lambda ()
+    (cons (lambda (search-var)
+	    (report-no-binding-found search-var))
+	  (lambda ()
+	    #t))))
+
+(define extend-env
+  (lambda (saved-var saved-val saved-env)
+    (cons (lambda (search-var)
+      (if (eqv? search-var saved-var)
+        saved-val
+        (apply-env saved-env search-var)))
+        (lambda () #f))))
+
+(define apply-env
+  (lambda (env search-var)
+    ((car env) search-var)))
+
+(define empty-env?
+  (lambda (env)
+    ((cdr env))))
+
+
+(define report-no-binding-found
+  (lambda (search-var)
+    (error 'apply-env "No binding for ~s" search-var)))
+
+(define report-invalid-env
+  (lambda (env)
+    (error 'apply-env "Bad environment: ~s" env)))
+
+
+(define e
+  (extend-env 'd 6
+     (extend-env 'y 8
+        (extend-env 'x 7
+           (extend-env 'y 14
+              (empty-env))))))
+
+(equal?? (apply-env e 'd) 6)
+(equal?? (apply-env e 'y) 8)
+(equal?? (apply-env e 'x) 7)
+
+(equal?? (empty-env? (empty-env)) #t)
+(equal?? (empty-env? e) #f)
+
+(exit)
---
+++ eopl3/ch2/14.scm
@@ -0,0 +1,65 @@
+(load "../libs/init.scm")
+
+
+(define empty-env
+  (lambda ()
+    (list (lambda (search-var)
+	    (report-no-binding-found search-var))
+	  (lambda (search-var)
+	    #f)
+	  (lambda ()
+	    #t))))
+
+
+(define extend-env
+  (lambda (saved-var saved-val saved-env)
+    (list (lambda (search-var)
+	    (if (eqv? search-var saved-var)
+		saved-val
+		(apply-env saved-env search-var)))
+	  (lambda (search-var)
+	    (if (eqv? search-var saved-var)
+		#t
+		(has-binding? saved-env search-var)))
+	  (lambda ()
+	    #f))))
+
+(define apply-env
+  (lambda (env search-var)
+    ((car env) search-var)))
+
+(define has-binding?
+  (lambda (env search-var)
+    ((cadr env) search-var)))
+
+(define empty-env?
+  (lambda (env)
+    ((caddr env))))
+
+(define report-no-binding-found
+  (lambda (search-var)
+    (error 'apply-env "No binding for ~s" search-var)))
+
+(define report-invalid-env
+  (lambda (env)
+    (error 'apply-env "Bad environment: ~s" env)))
+
+(define e
+  (extend-env 'd 6
+     (extend-env 'y 8
+        (extend-env 'x 7
+           (extend-env 'y 14
+              (empty-env))))))
+
+(equal?? (apply-env e 'd) 6)
+(equal?? (apply-env e 'y) 8)
+(equal?? (apply-env e 'x) 7)
+
+(equal?? (empty-env? (empty-env)) #t)
+(equal?? (empty-env? e) #f)
+
+(equal?? (has-binding? e 'd) #t)
+(equal?? (has-binding? e 'x) #t)
+(equal?? (has-binding? e 'm) #f)
+
+(exit)
