6ed253863d02c5681c4b3324d7c4623d09dbdc35
GarryChen <chenymj23@gmail.com>
2025-04-05 19:00:47 +0200

    print the welcome message

--- cs106B/HW1_Life/src/life.cpp
+++ cs106B/HW1_Life/src/life.cpp
@@ -17,8 +17,27 @@
 #include "lifegui.h"
 using namespace std;
 
+//Constant declerations (for further changes)
+const string WELCOME_MESSAGE = "Welcome to the CS 106B Game of Life,\n"
+                               "a simulation of the lifecycle of a bacteria colony.\n"
+                               "Cells (X) live and die by the following rules:\n"
+                               "- A cell with 1 or fewer neighbors dies.\n"
+                               "- Locations with 2 neighbors remain stable.\n"
+                               "- Locations with 3 neighbors will create life.\n"
+                               "- A cell with 4 or more neighbors dies.\n\n";
+const string PROMPT_FILE = "Grid input file name? ";
+const string RANDOM = "(type \"random\" to generate a random pattern) ";
+const string FILE_ERROR = "Unable to open that file.  Try again.\n";
+const string OPTIONS = "Should the simulation wrap around the grid (y/n)? ";
+const string MENU = "a)nimate, t)ick, q)uit? ";
+const string PROMPT_FRAME_NUMBER = "How many frames? ";
+const string ERROR = "Invalid choice; please try again.\n";
+const int PAUSE = 50;
+
 int main() {
-    // TODO: Finish the program!
+
+    // Displaying the intro welcome message
+    cout << WELCOME_MESSAGE;
 
 
     cout << "Have a nice Life!" << endl;
