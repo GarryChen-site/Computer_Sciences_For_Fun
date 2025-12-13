27db9528b48233db4b5bf918f03b0d3583306cec
GarryChen <chenymj23@gmail.com>
2025-12-13 19:53:18 +0900

    add vector class implementation with constructors, destructors, and member functions

---
+++ cs106L/L14-move-semantics/template-vector/main.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <deque>
+#include <iostream>
+
+#include "vector.h"
+using namespace mycollection;
+
+using std::cout;
+using std::endl;
+using std::string;
+
+vector<string> findAllWords(size_t size);
+
+int main() {
+  vector<string> words1 = findAllWords(54321234);
+  vector<string> words2;
+  words2 = findAllWords(54321234);
+  cout << "done!" << endl;
+}
+
+vector<string> findAllWords(size_t size) {
+  // do some file reading
+  vector<string> words(size, "Ito");
+  return words;
+}
